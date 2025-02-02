9ed062af78834163008f0062ab81b43fc37b36c9
Mike Hoyos <mikehoyos7@gmail.com>
2025-02-02 01:12:53 -0600

    creacion proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { CharactersService } from './shared/services/characters.service';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import { InterceptorService } from './shared/interceptor/interceptor.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent],
+  providers: [CharactersService, {provide:HTTP_INTERCEPTORS,useClass:InterceptorService, multi:true}],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'marvelAngular';
+}
